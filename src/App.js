f33f7482b275ebbf1bf51313ead3fbfad7e9a827
Ryan Akbar <ryanakbar.armial@gmail.com>
2020-09-14 20:21:33 +0700

    success fetch API

--- src/App.js
+++ src/App.js
@@ -1,50 +1,59 @@
 import React from 'react';
+import useFetch from './utils/useFetch.js';
 import './App.css';
 import './css/tailwind.css';
 
 function App() {
+  const [users, isLoading, error] = useFetch('https://randomuser.me/api/?results=10');
+  if (error) {
+    return <h1>Error</h1>;
+  }
+
   return (
     <div className="text-center">
       <header className="bg-gray-800 m-6 p-6 rounded shadow-lg">
         <h1 className="text-white text-3xl">Random User List</h1>
       </header>
-      <div class="container mx-auto">
-        <table class="table-auto">
-          <thead>
-            <tr>
-              <th class="px-4 py-2">ID</th>
-              <th class="px-4 py-2">Picture</th>
-              <th class="px-4 py-2">Name</th>
-              <th class="px-4 py-2">Email</th>
-              <th class="px-4 py-2">Phone Number</th>
-              <th class="px-4 py-2">Gender</th>
-              <th class="px-4 py-2">Location</th>
-              <th class="px-4 py-2">Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td class="border px-4 py-2">1</td>
-              <td class="border px-4 py-2">
-                <img
-                  className="rounded-full"
-                  src="https://randomuser.me/api/portraits/thumb/men/75.jpg"
-                  alt=""
-                />
-              </td>
-              <td class="border px-4 py-2">Mr. Brad Gibson</td>
-              <td class="border px-4 py-2">brad.gibson@example.com</td>
-              <td class="border px-4 py-2">081-454-0666</td>
-              <td class="border px-4 py-2">Male</td>
-              <td class="border px-4 py-2">Waterford, Kilcoole</td>
-              <td class="border px-4 py-2">
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                  ADD
-                </button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+      <div className="container mx-auto max-w-screen-lg">
+        {isLoading ? (
+          <h1>Loading...</h1>
+        ) : (
+          <table className="table-auto">
+            <thead>
+              <tr>
+                <th className="px-4 py-2">ID</th>
+                <th className="px-4 py-2">Picture</th>
+                <th className="px-4 py-2">Name</th>
+                <th className="px-4 py-2">Email</th>
+                <th className="px-4 py-2">Phone Number</th>
+                <th className="px-4 py-2">Gender</th>
+                <th className="px-4 py-2">Location</th>
+                <th className="px-4 py-2">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.results &&
+                users.results.map((user, i) => (
+                  <tr key={i.toString()}>
+                    <td className="border px-4 py-2">{i + 1}</td>
+                    <td className="border px-4 py-2">
+                      <img className="rounded-full" src={user.picture.thumbnail} alt="" />
+                    </td>
+                    <td className="border px-4 py-2">{`${user.name.title}. ${user.name.first} ${user.name.last}`}</td>
+                    <td className="border px-4 py-2">{user.email}</td>
+                    <td className="border px-4 py-2">{user.cell}</td>
+                    <td className="border px-4 py-2">{user.gender}</td>
+                    <td className="border px-4 py-2">{`${user.location.city}, ${user.location.country}`}</td>
+                    <td className="border px-4 py-2">
+                      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        ADD
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        )}
       </div>
     </div>
   );
---
+++ src/utils/useFetch.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default (url, options = null) => {
+  const [data, setData] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        const res = await fetch(url, options);
+        const json = await res.json();
+        setData(json);
+      } catch (e) {
+        setError(e);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+  return [data, isLoading, error];
+};
