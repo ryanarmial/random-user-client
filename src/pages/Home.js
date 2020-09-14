437dd2db75e90ecf6427f7a79b3ce984ebf6305d
Ryan Akbar <ryanakbar.armial@gmail.com>
2020-09-15 01:16:25 +0700

    add user and then redirect

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import useFetch from '../utils/useFetch.js';
 
 function Home() {
   const [users, isLoading, error] = useFetch('https://randomuser.me/api/?results=5');
+  const history = useHistory();
+
+  async function onClickAdd(user) {
+    const newUser = {
+      name: `${user.name.title}. ${user.name.first} ${user.name.last}`,
+      email: user.email,
+      phone_number: user.cell,
+      gender: user.gender,
+      location: `${user.location.city}, ${user.location.country}`,
+      picture: user.picture.thumbnail,
+    };
+
+    const response = await fetch('http://localhost:4000/users/', {
+      method: 'POST',
+      body: newUser,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    });
+    const { data } = await response.json();
+    history.push(`/users/${data[0]}`);
+  }
+
   if (error) {
     return <h1>Error</h1>;
   }
@@ -38,7 +62,10 @@ function Home() {
                   <td className="border px-4 py-2">{user.gender}</td>
                   <td className="border px-4 py-2">{`${user.location.city}, ${user.location.country}`}</td>
                   <td className="border px-4 py-2">
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    <button
+                      onClick={() => onClickAdd(user)}
+                      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    >
                       ADD
                     </button>
                   </td>
